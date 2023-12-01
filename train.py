db4768de726d8c4ff33c2c593fa245704ba93a2f
Himanshu Bag <himanshubag12@gmail.com>
2023-12-01 23:23:44 +0530

    resolved the issue/error faced during accuracy calculation during model eval

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils.clip_grad import clip_grad_norm_
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 import transformers
 import pandas as pd
 import numpy as np
@@ -9,6 +10,7 @@ import time
 import gc
 import traceback
 import datetime
+from tqdm import tqdm
 
 from dataset import TrainDataset
 from model import Model
@@ -21,7 +23,8 @@ def train_model(model, optimizer, scheduler, loss_function, epochs,train_data_lo
         model.train()
         for epoch in range(epochs):
             best_loss = []
-            for step,batch in enumerate(train_data_loader):
+            avg_acc = []
+            for step,batch in enumerate(tqdm(train_data_loader)):
                 batch_inputs, batch_masks, batch_labels = batch['ids'], batch['mask'], batch['targets']
                 batch_token_type_ids = batch['token_type_ids']
                 model.zero_grad()
@@ -29,11 +32,14 @@ def train_model(model, optimizer, scheduler, loss_function, epochs,train_data_lo
                 loss = loss_function(outputs.squeeze(),batch_labels.squeeze())
                 best_loss.append(loss)
                 loss.backward()
+                out = [np.argmax(i) for i in outputs.cpu().detach().numpy()]
+                acc = metrics.accuracy_score(out, batch_labels)
+                avg_acc.append(acc)
                 clip_grad_norm_(model.parameters(), clip_value)
                 optimizer.step()
                 scheduler.step()
-            loss_avg = np.mean(best_loss)
-            print("Epoch : {0} , loss : {1}".format(epoch,loss_avg))
+            loss_avg = sum(best_loss)/len(best_loss)
+            print("Epoch: {0} ,loss : {1} , accuracy :{2}".format(epoch,loss_avg,np.mean(avg_acc)))
         return model
     
     except Exception as e:
@@ -53,7 +59,8 @@ def evaluate_model(model,loss_function,valid_data_loader):
                 outputs = model(batch_inputs, batch_masks, batch_token_type_ids)
             loss = loss_function(outputs, batch_labels)
             valid_loss.append(loss.item())
-            acc = metrics.accuracy_score(outputs, batch_labels)
+            out = [np.argmax(i) for i in outputs.cpu().detach().numpy()]
+            acc = metrics.accuracy_score(out, batch_labels)
             valid_acc.append(acc)
         return valid_loss, valid_acc
 
@@ -64,15 +71,15 @@ def evaluate_model(model,loss_function,valid_data_loader):
 
 
 def cross_validation(df,num_classes,k_fold):
-    epochs = 10  # no of epochs 
+    epochs = 4  # no of epochs 
     MAX_LENGTH = 64 # maxium length of embeddings (input to model)
     tokenizer_path = "bert-base-uncased"  # add tokenizer
     model_path = "bert-base-uncased" # add model path
     train_batch_size = 32  # training batch size
-    learn_rate = 1e-5  # learning rate
+    learn_rate = 3e-5  # learning rate
 
     for fold in range(1,k_fold+1):
-        print('>>>> Starting for fold : ',fold)
+        print('\t>>>> STARTING FOR fold : ',fold)
         df_train = df[df['kfold'] != fold].reset_index(drop=True)
         df_valid = df[df['kfold'] == fold].reset_index(drop=True)
         
@@ -90,12 +97,11 @@ def cross_validation(df,num_classes,k_fold):
         )
         train_data_loader = torch.utils.data.DataLoader(train_dataset,batch_size=train_batch_size,shuffle=True)
         valid_data_loader = torch.utils.data.DataLoader(valid_dataset,batch_size=train_batch_size,shuffle=True)
-        print('>>>> Train and Valid Dataset loaded')
+        print('Train and Valid Dataset loaded')
 
         num_train_steps = len(train_data_loader) * epochs
         model = Model(model_path=model_path,num_classes=num_classes)
-        print('>>>> Model Intialized')
-
+        print('Model Intialized')
         optimizer = torch.optim.AdamW(model.parameters(),lr=learn_rate,eps=1e-8)
         loss_function = nn.CrossEntropyLoss()
         scheduler = transformers.get_linear_schedule_with_warmup(
@@ -103,21 +109,23 @@ def cross_validation(df,num_classes,k_fold):
             num_warmup_steps=0,
             num_training_steps=num_train_steps
         )
-        
+
         xx = time.time()
-        print('>>>> Starting Model Train :- ',datetime.datetime.now())
+        print(' Starting Model Train :- ',datetime.datetime.now())
         model = train_model(model, optimizer, scheduler, loss_function, epochs,train_data_loader)
-        print('>>>> Model Train time taken :- ',time.time()-xx)
+        print(' Model Train time taken :- ',time.time()-xx)
+        torch.save(model.state_dict(),f'./model/model_{model_path}_fold-{fold}')
+        print('saved model to :- ',f'./model/model_{model_path}_fold-{fold}')
         if model is not None:
             valid_loss, valid_acc = evaluate_model(model,loss_function,valid_data_loader)
-            print('>>>> Validation loss :- ',valid_loss)
-            print('>>>> Validation accuracy :- ',valid_acc)   
+            print("valid_loss :-",valid_loss)
+            print('>> avg validation accuracy :',np.mean(valid_acc))
+            print("valid accuracy:-",valid_acc)
         else:
-            print("!!!!!!! Training Failed !!!!!!!")
-
+            print("!!!!!!! Training Failed , skipping validation !!!!!!!")
+        print('\n\n')
         del model
         gc.collect()
-        break
 
    
 
